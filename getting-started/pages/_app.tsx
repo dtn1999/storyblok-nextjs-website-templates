42ab2bb78b00ed71c6073702d7b10d20d9c5f0ed
Danyls Ngongang Tchekambou <danyls.tchekambou@esentri.com>
2022-11-29 12:30:59 +0100

    make some changes

--- getting-started/pages/_app.tsx
+++ getting-started/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+
+storyblokInit({
+  accessToken: String(process.env.NEXT_PUBLIC_STORY_BLOK_ACCESS_TOKEN),
+  use: [apiPlugin],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
